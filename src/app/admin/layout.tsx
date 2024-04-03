d12f7eeab13067364b7dbed249c52809f00fc8c2
DalpatRathore <69510006+DalpatRathore@users.noreply.github.com>
2024-04-03 13:07:35 +0530

    feat: store dashboard

---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,16 @@
+import Nav, { NavLinks } from "@/components/Nav";
+
+const AdminLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <Nav>
+        <NavLinks href={"/admin"}>Dashboard</NavLinks>
+        <NavLinks href={"/admin/products"}>Products</NavLinks>
+        <NavLinks href={"/admin/users"}>Users</NavLinks>
+        <NavLinks href={"/admin/orders"}>Sales</NavLinks>
+      </Nav>
+      <div className="container my-6">{children}</div>
+    </>
+  );
+};
+export default AdminLayout;
---
+++ src/app/admin/page.tsx
@@ -0,0 +1,106 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import db from "@/db/db";
+import { formatCurrency, formatNumber } from "@/lib/formatters";
+
+const getSalesData = async () => {
+  const data = await db.order.aggregate({
+    _sum: {
+      pricePaidInCents: true,
+    },
+    _count: true,
+  });
+  return {
+    amount: (data._sum.pricePaidInCents || 0) / 100,
+    numberOfSales: data._count,
+  };
+};
+
+const getUserData = async () => {
+  //   const userCount = await db.user.count();
+  //   const orderData = await db.order.aggregate({
+  //     _sum: { pricePaidInCents: true },
+  //   });
+
+  const [userCount, orderData] = await Promise.all([
+    db.user.count(),
+    db.order.aggregate({
+      _sum: { pricePaidInCents: true },
+    }),
+  ]);
+
+  return {
+    userCount,
+    avgValPerUser:
+      userCount === 0
+        ? 0
+        : (orderData._sum.pricePaidInCents || 0) / userCount / 100,
+  };
+};
+
+const getProductData = async () => {
+  const [activeCount, inActiveCount] = await Promise.all([
+    db.product.count({ where: { inStock: true } }),
+    db.product.count({ where: { inStock: false } }),
+  ]);
+
+  return { activeCount, inActiveCount };
+};
+
+const AdminDashboardPage = async () => {
+  //   const salesData = await getSalesData();
+  //   const userData = await getUserData();
+
+  const [salesData, userData, productData] = await Promise.all([
+    getSalesData(),
+    getUserData(),
+    getProductData(),
+  ]);
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <DashboardCard
+        title="Sales"
+        description={`${formatNumber(salesData.numberOfSales)} Orders`}
+        content={formatCurrency(salesData.amount)}
+      ></DashboardCard>
+      <DashboardCard
+        title="Customer"
+        description={`${formatNumber(userData.avgValPerUser)} Average Value`}
+        content={formatCurrency(userData.userCount)}
+      ></DashboardCard>
+      <DashboardCard
+        title="Active Products"
+        description={`${formatNumber(productData.inActiveCount)} Inactive`}
+        content={formatNumber(productData.activeCount)}
+      ></DashboardCard>
+    </div>
+  );
+};
+export default AdminDashboardPage;
+
+type DashboardCardProps = {
+  title: string;
+  description: string;
+  content: string;
+};
+
+export const DashboardCard = ({
+  title,
+  description,
+  content,
+}: DashboardCardProps) => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+        <CardDescription>{description}</CardDescription>
+      </CardHeader>
+      <CardContent>{content}</CardContent>
+    </Card>
+  );
+};
