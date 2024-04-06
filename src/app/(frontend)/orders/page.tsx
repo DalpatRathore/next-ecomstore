abb91dc459fd83fd03943ed147ad7d9575eb672b
DalpatRathore <69510006+DalpatRathore@users.noreply.github.com>
2024-04-06 16:48:18 +0530

    feat: order history with email

---
+++ src/app/(frontend)/orders/page.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import emailOrderHistory from "@/actions/order";
+import PageHeader from "@/app/admin/_components/PageHeader";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useFormState, useFormStatus } from "react-dom";
+
+const OrdersPage = () => {
+  const [data, action] = useFormState(emailOrderHistory, {});
+  return (
+    <>
+      <div className="flex justify-between items-center gap-4 border-b-2 mb-10">
+        <PageHeader>My Orders</PageHeader>
+      </div>
+      <form
+        action={action}
+        className="max-w-3xl mx-auto p-5 md:p-8 border rounded-lg"
+      >
+        <Card>
+          <CardHeader>
+            <CardTitle>My Orders</CardTitle>
+            <CardDescription>
+              Enter your email and we will email you your order history and
+              download links
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className="space-y-2">
+              <Label htmlFor="email">Email</Label>
+              <Input type="email" required name="email" id="email"></Input>
+            </div>
+            {data.error && <div className="text-destructive">{data.error}</div>}
+          </CardContent>
+          <CardFooter>
+            {data.message ? (
+              <p className="textemerald-500">{data.message}</p>
+            ) : (
+              <SubmitButton></SubmitButton>
+            )}
+          </CardFooter>
+        </Card>
+      </form>
+    </>
+  );
+};
+export default OrdersPage;
+
+const SubmitButton = () => {
+  const { pending } = useFormStatus();
+
+  return (
+    <Button type="submit" className="w-full" size={"lg"} disabled={pending}>
+      {pending ? "Sending..." : "Send"}
+    </Button>
+  );
+};
--- src/emails/OrderHistory.tsx
+++ src/emails/OrderHistory.tsx
@@ -1,4 +1,79 @@
-const OrderHistory = () => {
-  return <div>OrderHistory</div>;
+import {
+  Body,
+  Container,
+  Head,
+  Heading,
+  Hr,
+  Html,
+  Preview,
+  Tailwind,
+} from "@react-email/components";
+import OrderInformation from "./_components/OrderInformation";
+import React from "react";
+
+type OrderHistoryProps = {
+  orders: {
+    id: string;
+    createdAt: Date;
+    pricePaidInCents: number;
+    downloadVerificationId: string;
+    product: { name: string; description: string; imagePath: string };
+  }[];
+};
+
+const OrderHistory = ({ orders }: OrderHistoryProps) => {
+  return (
+    <Html>
+      <Preview>Order History & Download</Preview>
+      <Tailwind>
+        <Head />
+        <Body className="font-sans bg-white">
+          <Container className="max-w-xl">
+            <Heading>Order History</Heading>
+            {orders.map((order, i) => (
+              <React.Fragment key={order.id}>
+                <OrderInformation
+                  order={order}
+                  product={order.product}
+                  downloadVerificationId={order.downloadVerificationId}
+                ></OrderInformation>
+                {i < orders.length - 1 && <Hr className="my-8"></Hr>}
+              </React.Fragment>
+            ))}
+          </Container>
+        </Body>
+      </Tailwind>
+    </Html>
+  );
 };
 export default OrderHistory;
+
+OrderHistory.PreviewProps = {
+  orders: [
+    {
+      id: crypto.randomUUID(),
+      createdAt: new Date(),
+      pricePaidInCents: 1000,
+      downloadVerificationId: crypto.randomUUID(),
+      product: {
+        name: "Dalpat Rathore",
+        imagePath:
+          "/products/6a612d85-4ec5-410b-8a42-41020a1a89af-dalpatrathore.jpg",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, veritatis.",
+      },
+    },
+    {
+      id: crypto.randomUUID(),
+      createdAt: new Date(),
+      pricePaidInCents: 2000,
+      downloadVerificationId: crypto.randomUUID(),
+      product: {
+        name: "Dalpat Singh Rathore",
+        imagePath: "/products/8771b9a4-16ea-45c0-bd66-dbeef2c537a7-213.jpg",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, veritatis.",
+      },
+    },
+  ],
+} satisfies OrderHistoryProps;
