13ba79211ff53a7281c5a0f4013dedba73068dac
DalpatRathore <69510006+DalpatRathore@users.noreply.github.com>
2024-04-17 20:32:33 +0530

    style: error fallback ui

--- src/app/(frontend)/error.tsx
+++ src/app/(frontend)/error.tsx
@@ -1,5 +1,6 @@
 "use client"; // Error components must be Client Components
 
+import { Button } from "@/components/ui/button";
 import { useEffect } from "react";
 
 export default function Error({
@@ -15,16 +16,29 @@ export default function Error({
   }, [error]);
 
   return (
-    <div>
-      <h2>Something went wrong!</h2>
-      <button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
-      >
-        Try again
-      </button>
+    <div className="w-full flex flex-col lg:flex-row items-start justify-center px-5  max-w-7xl mx-auto gap-5 relative ">
+      <div className="p-5 md:p-10 w-full  max-w-7xl mt-5 md:mt-10 bg-white dark:bg-black">
+        <div className="w-full mx-auto max-w-3xl flex flex-col items-center justify-center gap-5">
+          <h2 className="w-full  scroll-m-20 text-lg sm:text-2xl md:text-3xl font-semibold tracking-tight text-center pl-5">
+            Something went wrong.
+          </h2>
+          <p className="leading-6 sm:leading-7 text-sm sm:text-base">
+            We apologize for the inconvenience.
+          </p>
+
+          <Button variant="outline" onClick={() => location.reload()}>
+            Refresh Page
+          </Button>
+
+          <p className="leading-6 sm:leading-7 text-sm sm:text-base">
+            If the issue persists.
+          </p>
+          <p className="leading-6 sm:leading-7 text-sm sm:text-base">
+            Kindly get in touch with the website administrator for further
+            assistance.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
--- src/app/admin/error.tsx
+++ src/app/admin/error.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+
 export default function Error({
   error,
   reset,
@@ -8,10 +10,29 @@ export default function Error({
   reset: () => void;
 }) {
   return (
-    <div>
-      <h2>Something went wrong! admin</h2>
+    <div className="w-full flex flex-col lg:flex-row items-start justify-center px-5  max-w-7xl mx-auto gap-5 relative ">
+      <div className="p-5 md:p-10 w-full  max-w-7xl mt-5 md:mt-10 bg-white dark:bg-black">
+        <div className="w-full mx-auto max-w-3xl flex flex-col items-center justify-center gap-5">
+          <h2 className="w-full  scroll-m-20 text-lg sm:text-2xl md:text-3xl font-semibold tracking-tight text-center pl-5">
+            Something went wrong.
+          </h2>
+          <p className="leading-6 sm:leading-7 text-sm sm:text-base">
+            We apologize for the inconvenience.
+          </p>
 
-      <button onClick={() => reset()}>Try again</button>
+          <Button variant="outline" onClick={() => location.reload()}>
+            Refresh Page
+          </Button>
+
+          <p className="leading-6 sm:leading-7 text-sm sm:text-base">
+            If the issue persists.
+          </p>
+          <p className="leading-6 sm:leading-7 text-sm sm:text-base">
+            Kindly get in touch with the website administrator for further
+            assistance.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
